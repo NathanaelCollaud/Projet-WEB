c3986c5ef6adfa064448e32396f3fae4ec19b898
zutzut333 <61789885+zutzut333@users.noreply.github.com>
2021-03-22 10:45:53 +0100

    3

--- SiteJeanNat/ProjetWebJeanNat/src/view/home.php
+++ SiteJeanNat/ProjetWebJeanNat/src/view/home.php
@@ -9,16 +9,55 @@
 
 ob_start();
 $title = "Accueil";
+$NIF = "view/content/img/contact-us.jpg";
+
+
 ?>
+
+    <head>
+
+        <script src="/ProjetWebJeanNat/src/data/BaseData.json"></script>
+
+    </head>
 
     <body>
 
+
     <div>
+
+        <select datasrc=""></select>
+
+
+        <select name="tri" class="AccueilTri">
+
+            <option value="Date(récent)">Date (récent)</option>
+            <option value="Date(ancien)">Date (ancien)</option>
+
+        </select>
+
+        <select>
+
+
+
+        </select>
+
+
         <img src="view/content/img/logoPintEdit.png"/>
 
+        <a class="AccueilBouton" href="Account.php"> Compte </a>
+
         <h1 class="centerTitreAccueil">PintEdit</h1>
+
+        <h3 class="AccueilTrierPar"> Trier par</h3>
+
+        <h4 class="AccueilFiltre"> Filtre</h4>
+
     </div>
 
+    <div>
+
+
+    </div>
 
 
 
