0e20e31af92bff80fe8170eadbf40b2de71a8b62
zutzut333 <61789885+zutzut333@users.noreply.github.com>
2021-04-07 00:20:33 +0200

    cqr

--- SiteJeanNat/ProjetWebJeanNat/src/view/observation.php
+++ SiteJeanNat/ProjetWebJeanNat/src/view/observation.php
@@ -38,12 +38,34 @@ $title = "Observation";
         <img src="view/content/img/pink-contem-girl.jpg" class="imgObservation">
     </div>
 
-    <div>
-
+    <div class="lateraltext">
+        <h2>
+            Auteur
+        </h2>
+        <br>
+        <h2>
+            E-mail
+        </h2>
+        <br>
+        <h2>
+            Date
+        </h2>
+        <br>
+        <h2>
+            Lieu
+        </h2>
+        <br>
+        <h2>
+            Animal(aux)
+        </h2>
     </div>
 
-    <div>
+    <div class="mapObservation">
+            <h2>map</h2>
+    </div>
 
+    <div class="DescriptionObservation">
+        <h2>Description</h2>
     </div>
 
 </body>
